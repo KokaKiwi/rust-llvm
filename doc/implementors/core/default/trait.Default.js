7ee87d8a0717c99afd09f7463cfd4468edcc9405
KokaKiwi <kokakiwi@kokakiwi.net>
2015-01-12 02:49:12 +0100

    Update doc.

---
+++ doc/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['gxx'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='gxx/struct.Config.html' title='gxx::Config'>Config</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
