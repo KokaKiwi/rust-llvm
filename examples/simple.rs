5c8480498ed1eaec9b6bc9b93e5a1df14771e86d
KokaKiwi <kokakiwi@kokakiwi.net>
2015-01-13 19:56:12 +0100

    New example.

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,18 +2,27 @@
 extern crate llvm;
 
 use llvm::api;
-use llvm::api::ty::{Type};
-use llvm::api::ty::seq::{ArrayType};
-use llvm::api::value::{ValueExt};
-use llvm::api::value::user::constant::{Constant, ConstantArray};
+use llvm::api::{Module};
+use llvm::api::ty::{Type, FunctionType};
+use llvm::traits::*;
+
+fn create_main_fntype(ctx: &LLVMContextExt) -> FunctionType {
+    let ret_ty = Type::get_int32_ty(ctx);
+
+    let arg_tys = [
+        &Type::get_int32_ty(ctx) as &TypeExt,
+        &Type::get_int8_ptr_ty(ctx) as &TypeExt,
+    ];
+
+    FunctionType::get(&ret_ty, &arg_tys, false)
+}
 
 fn main() {
     let ctx = api::get_global_context();
+    let mut m = Module::new("simple", &ctx);
 
-    let ty = Type::get_int16_ty(&ctx);
-    let value = Constant::get_integer_value(&ty, (16, &[42]));
+    let main_fn_ty = create_main_fntype(&ctx);
+    let function = m.get_or_insert_function("main", &main_fn_ty);
 
-    let array_ty = ArrayType::get(&ty, 2);
-    let array = ConstantArray::get(&array_ty, &[&value, &value]);
-    array.dump();
+    function.dump();
 }
