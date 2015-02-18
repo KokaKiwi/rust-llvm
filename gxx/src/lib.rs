5087c8fc386d8e433817f01fd7e58340b7b98e2b
asdf <asdf@debian>
2015-02-18 03:05:08 -0500

    Upgrading gxx for latest rust version

--- gxx/src/lib.rs
+++ gxx/src/lib.rs
@@ -1,8 +1,10 @@
 #![allow(unstable)]
 use std::default::Default;
-use std::io::Command;
-use std::io::process::ProcessExit;
+use std::process::Command;
+use std::process::ExitStatus;
 use std::os;
+// XXX_ How to set permissions the portable way
+use std::os::unix::prelude::PermissionsExt;
 
 pub struct Config {
     pub include_dirs: Vec<Path>,
@@ -29,51 +31,55 @@ pub fn compile_library(name: &str, config: &Config, sources: &[&str]) {
     let root_dir = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap());
     let out_dir = Path::new(os::getenv("OUT_DIR").unwrap());
 
-    let mut cmd = Command::new(gxx);
-    cmd.arg(format!("-O{}", opt_level));
-    cmd.args(&["-c", "-ffunction-sections", "-fdata-sections"]);
-
-    for include_dir in config.include_dirs.iter() {
-        cmd.arg("-I").arg(include_dir);
-    }
-
-    for &(ref key, ref value) in config.definitions.iter() {
-        if let &Some(ref value) = value {
-            cmd.arg(format!("-D{}={}", key, value));
-        } else {
-            cmd.arg(format!("-D{}", key));
-        }
-    }
-
-    cmd.args(&config.flags[]);
-
     let mut objects = Vec::new();
     for source in sources.iter() {
+        let mut cmd = Command::new(&gxx);
+        cmd.arg(&format!("-O{}", opt_level));
+        cmd.args(&["-c", "-ffunction-sections", "-fdata-sections"]);
+
+        for include_dir in config.include_dirs.iter() {
+            cmd.arg("-I").arg(include_dir);
+        }
+
+        for &(ref key, ref value) in config.definitions.iter() {
+            if let &Some(ref value) = value {
+                cmd.arg(&format!("-D{}={}", key, value));
+            } else {
+                cmd.arg(&format!("-D{}", key));
+            }
+        }
+
+        cmd.args(&config.flags[]);
+
         let object = out_dir.join(*source).with_extension("o");
-        ::std::io::fs::mkdir_recursive(&object.dir_path(), ::std::io::USER_RWX).unwrap();
-        run(cmd.clone().arg(root_dir.join(*source)).arg("-o").arg(&object));
+        ::std::fs::create_dir_all(&object.dir_path());
+        let mut perms = ::std::fs::metadata(&object.dir_path()).unwrap().permissions();
+        perms.set_mode(0x777);
+        ::std::fs::set_permissions(&object.dir_path(), perms).unwrap();
+        
+        run(cmd.arg(&root_dir.join(*source)).arg("-o").arg(&object));
 
         objects.push(object);
     }
 
-    run(Command::new(ar)
+    run(Command::new(&ar)
         .arg("crus")
-        .arg(out_dir.join(output))
+        .arg(&out_dir.join(output))
         .args(&objects[]));
 
     println!("cargo:rustc-flags=-L native={} -l {}:static", out_dir.display(), name);
 }
 
-fn run(cmd: &Command) -> (String, String, ProcessExit) {
-    println!("running: {}", cmd);
+fn run(cmd: &mut Command) -> (String, String, ExitStatus) {
+    println!("running: {:?}", cmd);
 
     let mut process = cmd.spawn().unwrap();
 
     let status = process.wait().unwrap();
     let output = process.wait_with_output().unwrap();
 
-    let stdout = String::from_utf8(output.output).unwrap();
-    let stderr = String::from_utf8(output.error).unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let stderr = String::from_utf8(output.stderr).unwrap();
 
     if !status.success() {
         panic!("nonzero exit status: {}\n{}", status, stderr);
